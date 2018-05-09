be691c32a15a9c6e90800b8143aa73827eed702c
Tuong Van Nguyen <tvan262@yahoo.com>
2018-05-09 19:30:05 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  
+  if name_hash.length == 0
+    return nil
+  else
+    array = []
 
 end
